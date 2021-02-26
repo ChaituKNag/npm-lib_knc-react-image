090bd7a3ef977a732cd100dfea3eca2cdc10cdd9
K.N.C <knc@outlook.in>
2021-02-26 23:27:28 +0530

    backup

--- example/src/index.js
+++ example/src/index.js
@@ -6,14 +6,14 @@ const list = [
     'https://picsum.photos/600?image=200',
     'https://picsum.photos/600?image=201',
     'https://picsum.photos/600?image=202',
-    // 'https://picsum.photos/600?image=203',
-    // 'https://picsum.photos/600?image=204',
-    // 'https://picsum.photos/600?image=215',
-    // 'https://picsum.photos/600?image=206',
-    // 'https://picsum.photos/600?image=217',
-    // 'https://picsum.photos/600?image=208',
-    // 'https://picsum.photos/600?image=209',
-    // 'https://picsum.photos/600?image=210',
+    'https://picsum.photos/600?image=203',
+    'https://picsum.photos/600?image=204',
+    'https://picsum.photos/600?image=215',
+    'https://picsum.photos/600?image=206',
+    'https://picsum.photos/600?image=217',
+    'https://picsum.photos/600?image=208',
+    'https://picsum.photos/600?image=209',
+    'https://picsum.photos/600?image=210',
 ]
 
 const App = () => {
@@ -23,7 +23,10 @@ const App = () => {
             maxWidth: "600px"
         }}>
             {
-                list.map(image => <Image key={image} src={image} width={600} height={600}/>)
+                list.map(image => {
+                    return <Image key={image} src={image} width={600} height={600}/>;
+                    // return <img src={image} key={image} />
+                })
             }
         </div>
     )
